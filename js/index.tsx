2a60cbe3affa5a44089c41f846f3f6741ad571c6
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-04-22 01:26:13 +0200

    print version to console

--- js/index.tsx
+++ js/index.tsx
@@ -2,10 +2,12 @@ import React, {Fragment} from 'react'
 import {createRoot} from 'react-dom/client'
 import Style from './Style'
 import Routed from './Routed'
+import pkgInfo from '../package.json'
 
 
 /** @returns {Fragment} */
 function Root({children}) {
+  console.log(`Celestiary version: ${pkgInfo.version}`)
   return (
     <Style>
       <Routed/>
