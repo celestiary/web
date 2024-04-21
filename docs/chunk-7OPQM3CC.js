ea799ac7aa468d8b7e443b9582eab27265666dbc
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-04-21 23:26:45 +0200

    yarn upgrade

---
+++ docs/chunk-7OPQM3CC.js
@@ -0,0 +1,2 @@
+import{b as n,g as i}from"./chunk-5LDLQYXE.js";var a=n(t=>{"use strict";var e=i();t.createRoot=e.createRoot,t.hydrateRoot=e.hydrateRoot;var s});export{a};
+//# sourceMappingURL=chunk-7OPQM3CC.js.map
